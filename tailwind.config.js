524e38a7fcdd65c7c9c20cd443b95b6affd23b68
MrRam-X <ramprasadbaidya21@gmail.com>
2025-09-12 15:05:45 +0530

    feat: repo initialised with necessary packages

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+      },
+      colors: {
+        "brand-gold": "#f5b754",
+        "brand-dark": "#121212",
+        "brand-gray": "#666666",
+      },
+    },
+  },
+  plugins: [],
+};
