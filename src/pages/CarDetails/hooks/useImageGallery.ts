c7ccc8efd4291872efe2f49cc6208d9f11074704
MrRam-X <ramprasadbaidya21@gmail.com>
2025-11-11 23:41:57 +0530

    fix: image not showing when clicking on left arrow of image gallery

--- src/pages/CarDetails/hooks/useImageGallery.ts
+++ src/pages/CarDetails/hooks/useImageGallery.ts
@@ -2,24 +2,28 @@ import { useState } from "react";
 
 const useImageGallery = (imageGalleryArr: string[]) => {
   const [currIdx, setCurrIdx] = useState<number>(0); // Default index of image gallery is 0
-  const [isGalleryModalOpen, setIsGalleryModalOpen] = useState<boolean>(false)
+  const [isGalleryModalOpen, setIsGalleryModalOpen] = useState<boolean>(false);
 
   const nextImageButtonHandler = () => {
     setCurrIdx((prev) => (prev + 1) % imageGalleryArr.length);
   };
 
   const prevImageButtonHandler = () => {
-    setCurrIdx((prev) => (prev - 1) % imageGalleryArr.length);
+    setCurrIdx((prev) =>
+      prev === 0
+        ? imageGalleryArr.length - 1
+        : (prev - 1) % imageGalleryArr.length
+    );
   };
 
   const currentImageIdxHandler = (imageName: string) => {
     setCurrIdx(imageGalleryArr.findIndex((item) => item === imageName) || 0);
-    if(!isGalleryModalOpen) setIsGalleryModalOpen(true)
+    if (!isGalleryModalOpen) setIsGalleryModalOpen(true);
   };
 
   const closeGalleryModal = () => {
-    setIsGalleryModalOpen(false)
-  }
+    setIsGalleryModalOpen(false);
+  };
 
   return {
     currIdx,
